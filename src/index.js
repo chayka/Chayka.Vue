49d2f0f53196cbc6ecfbd77a99f0c1ad9e24749b
Boris Mossounov <borix@tut.by>
2017-02-09 13:40:29 +0300

    Fixing exports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import Vue from 'vue';
 import modals from './modals';
 import Modal from './Components/Modal.vue';
 import Modals from './Components/Modals.vue';
-
 import spinners from './spinners';
 import Spinner from './Components/Spinner.vue';
 import Spinners from './Components/Spinners.vue';
@@ -13,3 +12,8 @@ Vue.component('modals', Modals);
 
 Vue.component('spinner', Spinner);
 Vue.component('spinners', Spinners);
+
+module.exports = {
+    Spinner, Spinners, spinners,
+    Modal, Modals, modals
+};
